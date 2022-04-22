ccd1ee80b1976c8f513380121eb51f7f85256d58
Cyrius58 <victor.mauboussin@outlook.fr>
2022-04-22 11:54:13 +0200

    Updates

---
+++ precache-manifest.f5b5e8947cff0b1ec5d6a210b9b0aa0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9e14c23e64b31ada31a1fea2b5277f",
+    "url": "/Daap/index.html"
+  },
+  {
+    "revision": "fa857018de797c1ea43a",
+    "url": "/Daap/static/css/main.27c09b67.chunk.css"
+  },
+  {
+    "revision": "9c5479421583959b9034",
+    "url": "/Daap/static/js/2.cf366a20.chunk.js"
+  },
+  {
+    "revision": "fa857018de797c1ea43a",
+    "url": "/Daap/static/js/main.17cb8e76.chunk.js"
+  },
+  {
+    "revision": "706ff8382847b8547d3a",
+    "url": "/Daap/static/js/runtime-main.558329f0.js"
+  }
+]);
